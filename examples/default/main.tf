faf26b5de0f00cf4abcfb9b979fa44d3d7e96853
Terry Mandin <terry.mandin@microsoft.com>
2026-01-06 12:34:17 -0700

    Update provider versions

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.74"
+      version = "~> 4.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5"
+      version = "~> 3.7"
     }
   }
 }
