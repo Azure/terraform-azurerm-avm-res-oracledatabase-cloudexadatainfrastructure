1a274b40ac26716ee7cfcb3b5fb9a6ece66fba17
Terry Mandin <terry.mandin@microsoft.com>
2026-01-06 13:19:16 -0700

    linting fixes

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -36,7 +36,7 @@ locals {
 # This ensures we have unique CAF compliant names for our resources.
 module "naming" {
   source  = "Azure/naming/azurerm"
-  version = "~> 0.3"
+  version = "0.4.3"
 }
 
 resource "random_string" "suffix" {
